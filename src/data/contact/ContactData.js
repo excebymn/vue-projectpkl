d36c1214e3cae5029c2a2bad08bf24408cc3608b
excebymn <excebymn@gmail.com>
2026-06-18 17:33:49 +0700

    menambahkan data contact

--- src/data/contact/ContactData.js
+++ src/data/contact/ContactData.js
@@ -3,40 +3,60 @@ export const contactData = [
   {
     title: 'WhatsApp',
     description: 'Chat langsung dengan tim kami',
-    username: 'Respon Cepat',
+    username: 'Respon cepat via WhatsApp',
     icon: 'bi-whatsapp',
     iconBg: 'bg-success-subtle',
     iconColor: 'text-success',
-    link: 'https://wa.me/6287846395250',
+    link: 'https://api.whatsapp.com/send/?phone=6281331755314&text&type=phone_number&app_absent=0',
   },
 
   {
     title: 'Instagram',
     description: 'Aktivitas & update terbaru',
-    username: '@username',
+    username: '@makna_consulting',
     icon: 'bi-instagram',
     iconBg: 'bg-danger-subtle',
     iconColor: 'text-danger',
-    link: 'https://instagram.com/username',
+    link: 'https://www.instagram.com/makna_consulting',
   },
 
   {
     title: 'TikTok',
     description: 'Konten singkat & insight',
-    username: '@username',
+    username: '@makna_consulting',
     icon: 'bi-tiktok',
     iconBg: 'bg-dark-subtle',
     iconColor: '',
-    link: 'https://tiktok.com/@username',
+    link: 'https://www.tiktok.com/@makna_consulting',
   },
 
   {
     title: 'Email',
     description: 'Untuk komunikasi formal',
-    username: 'email@makna.com',
+    username: 'makna.team@gmail.com',
     icon: 'bi-envelope-fill',
     iconBg: 'bg-warning-subtle',
     iconColor: 'text-warning',
-    link: 'mailto:email@makna.com',
+    link: 'mailto:makna.team@gmail.com',
+  },
+
+  {
+    title: 'Facebook',
+    description: 'Update & komunitas',
+    username: 'Makna Consulting',
+    icon: 'bi-facebook',
+    iconBg: 'bg-primary-subtle',
+    iconColor: 'text-primary',
+    link: 'https://www.facebook.com/share/1EKQUkTcjZ/',
+  },
+
+  {
+    title: 'YouTube',
+    description: 'Video & konten edukasi',
+    username: '@maknaconsulting',
+    icon: 'bi-youtube',
+    iconBg: 'bg-danger-subtle',
+    iconColor: 'text-danger',
+    link: 'https://youtube.com/@maknaconsulting',
   },
 ]
